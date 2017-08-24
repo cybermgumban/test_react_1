db02859026932fe164b6acac981cc1892a82e0ea
Marlon Gumban <msg2msg2@gmail.com>
2017-08-24 08:17:46 +0800

    Made my test program worked!! :)

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -10,8 +10,15 @@ class Detail extends React.Component {
     constructor(props) {
         super(props);
 
+        this.renderCommits = this.renderCommits.bind(this);
+        this.renderForks = this.renderForks.bind(this);
+        this.renderPulls = this.renderPulls.bind(this);
+
         this.state = {
-            commits: []
+            result: [],
+            commits: [],
+            forks: [],
+            pulls: []
         };
     }
 
@@ -19,42 +26,101 @@ class Detail extends React.Component {
         ajax.get("https://api.github.com/repos/facebook/react/commits")
         .end((error, response) => {
             if (!error && response) {
-                console.dir(response.body);
+                console.dir(response.body)
                 this.setState({ commits: response.body });
             } else {
-                console.log("There was an error fetching fro Github", error)
+                console.log("There was an error fetching from Github", error)
             }
         })
-    }
 
-    handleBtnClick() {
-        ajax.get("https://api.github.com/repos/facebook/react/commits")
+        ajax.get("https://api.github.com/repos/facebook/react/forks")
         .end((error, response) => {
-            if (!error && response) {
+            if(!error && response) {
                 console.dir(response.body);
-                this.setState({ commits: response.body });
+                this.setState({ forks: response.body });
             } else {
-                console.log("There was an error fetching fro Github", error)
+                console.log("There was an error fetching from Github", error)
             }
         })
+
+        ajax.get("https://api.github.com/repos/facebook/react/pulls")
+        .end((error, response) => {
+            if(!error && response) {
+                console.dir(response.body);
+                this.setState ({ pulls: response.body });
+            } else {
+                console.log("There was an error fetching from Github")
+            }
+        }) 
     }
 
-    render() {
-        return (
+        renderCommits() {
+            console.log("Pressed Commit");
+            this.setState( {result:
             <div>
-                <button onClick={this.handleBtnClick.bind(this)}>
-                    Refresh!
-                </button>
                 {this.state.commits.map((commit, index) => (
                     <p key={index}>
                         <strong>
                             {commit.author ? commit.author.login : "Anonymous"}:
                         </strong><br />
-                        <a href="commit.html_url}">
+                        <a href="commit.html_url">
                             {commit.commit.message}
                         </a>
                     </p>
                 ))}
+            </div>
+        });
+        }
+
+        renderForks() {
+            console.log("Pressed Forks");
+            this.setState( {result:
+            <div>
+                {this.state.forks.map((forks, index) => (
+                    <p key={index}>
+                        <strong>
+                            {forks.default_branch}:
+                        </strong><br />
+                        <a href="forks.deployments_url">
+                            {forks.description}
+                        </a>
+                    </p>
+                ))}
+            </div>
+            });     
+        }        
+
+        renderPulls() {
+            console.log("Pressed Pulls");
+            this.setState( {result:
+            <div>
+                {this.state.pulls.map((pulls, index) => (
+                    <p key={index}>
+                        <strong>
+                            {pulls.user.login ? pulls.user.login : "Anonymous"}:
+                        </strong><br />
+                        <a href={"pulls.html_url"}>
+                            {pulls.id}
+                        </a>
+                    </p>
+                ))}
+            </div>
+            });    
+        } 
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.renderCommits}>
+                    Commits
+                </button>
+                <button onClick={this.renderForks}>
+                    Forks
+                </button>
+                <button onClick={this.renderPulls}>
+                    Pulls
+                </button> <br />
+                {this.state.result}
             </div>
         );
     }
