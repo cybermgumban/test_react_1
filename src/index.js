81fb98aa9430f6ffaff40286c16d38003a2d1bd5
Marlon Gumban <msg2msg2@gmail.com>
2017-08-25 07:36:12 +0800

    installed react-router and import it

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var Detail = require("./pages/Details");
+var {Router, Route, IndexRoute, useRouterHistory} = require("react-router");
+var {createHashHistory} = require("history");
 
 ReactDOM.render (
     <Detail />,
     document.getElementById("root")
-)
+) 
