5b67deb95b25f053c97b8c4b385cd64f9040d3a0
Marlon Gumban <msg2msg2@gmail.com>
2017-08-26 10:52:32 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var Detail = require("./pages/Details");
-var {Router, Route, IndexRoute, useRouterHistory} = require("react-router");
+var List = require("./pages/List");
+var {Router, Route, IndexRoute, useRouterHistory, hashHistory} = require("react-router");
 var {createHashHistory} = require("history");
 
+//npm install react-router@2.8.1
+//npm install history
+var appHistory = useRouterHistory(createHashHistory)({ queryKey:false})
+
 ReactDOM.render (
-    <Detail />,
+    <Router history={appHistory} onUpdate={()=>window.scrollTo(0,0)}>
+        <Route path="/" component={List} />
+        <Route path="/react" component={Detail} />
+    </Router>,
     document.getElementById("root")
-) 
+)
